31f6f5002509e400867410070b75ada5aee7cf8a
Iksan Arsana <ichzan21@users.noreply.github.com>
2026-07-14 23:21:15 +0800

    Keep Google login active until custom redirect is authorized

--- src/firebase.js
+++ src/firebase.js
@@ -13,7 +13,7 @@ import {
 
 const firebaseConfig = {
   apiKey: "AIzaSyC5C2wt3LyCHLY6SeH5HZ9FmdgzzkWL-LM",
-  authDomain: "www.aturduitku.com",
+  authDomain: "aturduitku.firebaseapp.com",
   projectId: "aturduitku",
   storageBucket: "aturduitku.firebasestorage.app",
   messagingSenderId: "965164237195",
